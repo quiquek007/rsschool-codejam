0ea795603e3f7ae9a60817e21cd14f615aa25239
quiquek007 <quiquek007@gmail.com>
2018-10-28 12:18:53 +0300

    refactor: change recursion test

--- recursion/tests/test.js
+++ recursion/tests/test.js
@@ -3,7 +3,7 @@ const recursion = require('../recursion.js');
 
 describe('recursion', () => {
   it('0', () => {
-    const tree = recursion({"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}});
+    const tree = recursion({ value: 100, left: { value: 90, left: { value: 70 }, right: { value: 99 } }, right: { value: 120, left: { value: 110 }, right: { value: 130 } } });
     assert.deepEqual(tree, [[100], [90, 120], [70, 99, 110, 130]]);
   });
 
@@ -13,7 +13,7 @@ describe('recursion', () => {
   });
 
   it('2', () => {
-    const tree = recursion({"value":100,"left":{"value":90,"left":{"value":70,"left":{"value":66}},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}});
+    const tree = recursion({ value: 100, left: { value: 90, left: { value: 70, left: { value: 66 } }, right: { value: 99 } }, right: { value: 120, left: { value: 110 }, right: { value: 130 } } });
     assert.deepEqual(tree, [[100], [90, 120], [70, 99, 110, 130], [66]]);
   });
 
